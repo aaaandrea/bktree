f7192b64e438af4eba3aeec5db483f5511a4f1f0
Andrea del Rio <adelrio@gmail.com>
2018-03-15 13:57:22 -0700

    add some tests

--- test/test_bktree.cpp
+++ test/test_bktree.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <numeric>
+#include "../src/main.cpp"
 
-void build_bktree()
+bool build_bktree()
 {
+    std::vector<std::string> dictionary = {"beep", "boop", "bing", "bong", "beet", "jingle", "jangle", "bee", "best", "peep", "belt"};
 
+    // try finding an iterator that goes through a txt file
+
+    auto tree = makeBKTree(dictionary.begin(), dictionary.end(), levenshtein_distance);
+    tree.printDotToStdout();
+
+    return true;
 }
 
-void import_tree()
+void load_tree()
 {
 
 }
 
 void lookup()
 {
-    
+    std::vector<std::string> dictionary = {"beep", "boop", "bing", "bong", "beet", "jingle", "jangle", "bee", "best", "peep", "belt"};
+    std::string value = "beep"
+
+    // try finding an iterator that goes through a txt file
+    auto tree = makeBKTree(dictionary.begin(), dictionary.end(), levenshtein_distance);
+    tree.nearest("beep", 2, "log");
+
+    return true;
+}
+
+int main()
+{
+    build_bktree();
+    load_tree();
+    lookup();
+    return 0;
 }
